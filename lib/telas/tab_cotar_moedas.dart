75e5f58dec0b7f82acfae3370d4560eed338fd99
André <andrejr.mello@hotmail.com>
2022-04-16 19:10:49 -0300

    Update tab_cotar_moedas.dart

--- lib/telas/tab_cotar_moedas.dart
+++ lib/telas/tab_cotar_moedas.dart
@@ -99,10 +99,7 @@ class _CotarAppState extends State<CotarMoedas> {
                     child: Container(
                       margin: const EdgeInsets.all(12),
                       padding: const EdgeInsets.all(24),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(
-                              color: Colors.amber.shade100, width: 3)),
+                      decoration: containerDecoration(),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
